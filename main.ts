cdb1dd8fe3416de2a50ee8187458d5e7f57d8098
Rodgers333 <102975802+Rodgers333@users.noreply.github.com>
2022-04-06 08:24:31 -0600

    Magic8

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    whatever = randint(0, 3)
+    if (whatever == 0) {
+        basic.showString("YES")
+    } else if (whatever == 1) {
+        basic.showString("NO")
+    } else if (whatever == 2) {
+        basic.showString("Who Knows")
+    } else {
+        basic.showString("Maybe Not")
+    }
+})
+let whatever = 0
+basic.showString("Ask A ?")
 basic.forever(function () {
 	
 })
